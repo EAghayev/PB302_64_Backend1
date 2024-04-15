463967349c9d4b142a9b02e661ffa242c208de20
Elchin Aghayev <Elchin@code.local>
2024-04-15 21:47:15 +0400

    project added

---
+++ MVC_firstApp/MVC_firstApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_firstApp.Controllers
+{
+    public class HomeController:Controller
+    {
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ firstApp/firstApp/Models/Group.cs
@@ -0,0 +1,9 @@
+﻿namespace firstApp.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string No { get; set; }
+        public byte Limit { get; set; }
+    }
+}
